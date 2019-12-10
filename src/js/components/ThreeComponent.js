4098341c9e98f8970cd3cb6b116250fc34daf314
Pouyaut Ludovic <ludovic.pouyaut@gmail.com>
2019-12-10 15:08:10 +0100

    add three scene

--- src/js/components/ThreeComponent.js
+++ src/js/components/ThreeComponent.js
@@ -1,10 +1,11 @@
 import bindAll from '../utils/bindAll';
-import THREE from 'three';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 class ThreeComponent {
     constructor(options) {
         bindAll(
-            this, 
+            this,
             '_tickHandler',
             '_resizeHandler'
         );
@@ -24,22 +25,45 @@ class ThreeComponent {
     _resize() {
         this._width = window.innerWidth;
         this._height = window.innerHeight;
+
+        // resize camera and renderer
+        this._camera.aspect = (this._width / this._height)
+        this._camera.updateProjectionMatrix();
+        this._renderer.setSize(this._width , this._height);
     }
 
     _setupThreeScene() {
-        
+        this._scene = new THREE.Scene();
+        this._camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+        this._renderer = new THREE.WebGLRenderer({
+            canvas: this.el
+        });
+
+        this._controls = new OrbitControls( this._camera, this._renderer.domElement );
+
+        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        this.cube = new THREE.Mesh( geometry, material );
+        this._scene.add( this.cube );
+
+        this._camera.position.z = 5;
+        this._controls.update()
     }
 
     _tick() {
-        
+        this._controls.update()
+        this._renderer.render(this._scene, this._camera);
     }
 
     _setupEventListeners() {
+        this._tickHandler();
         window.addEventListener('resize', this._resizeHandler);
     }
 
     _tickHandler() {
         this._tick();
+        window.requestAnimationFrame(this._tickHandler);
     }
 
     _resizeHandler() {
